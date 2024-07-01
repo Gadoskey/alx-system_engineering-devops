e4d9ea7b19f2dd7a3446af1838cca4a8a39e6e1a
gadoskey <gadoski@example.com>
2024-07-01 02:46:12 -0700

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,10 @@
+#creates a file in /tmp
+
 file { '/tmp/school':
-  content =>'I love Puppet',
-  mode    => '0744',
+  ensure  => 'file',
+  content => 'I love Puppet',
   owner   => 'www-data',
   group   => 'www-data',
+  mode    => '0744',
 }
+
