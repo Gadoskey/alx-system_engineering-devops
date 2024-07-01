68fa96a8514ed21b16d1f00083a3fe5dcf698a3b
gadoskey <gadoski@example.com>
2024-07-01 03:16:11 -0700

    stillWorking

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,6 @@
- # Ensure the python3-pip package is installed
-
-class flask {
-  package { 'python3-pip':
-    ensure => installed,
-  }
-
-# Install the Flask package using pip3 with the specified version
-
-exec { 'install_flask':
-    command => '/usr/bin/pip3 install flask==2.1.0',
-    unless  => '/usr/bin/pip3 show flask | grep -q "^Version: 2.1.0"',
-    require => Package['python3-pip'],
-  }
+#!/usr/bin/pup
+# Install flask version (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
