d3c586f11952c24a0ad99fd8d42c5536b96525ed
Gadoskey <gadoski321@gmail.com>
2024-07-30 04:10:13 -0700

    1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,13 +4,12 @@
     Returns to-do list information for a given employee ID.
 """
 
-import csv
-import requests
-import sys
+import csv, requests, sys
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    username = user.get("username")
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
     with open("{}.csv".format(sys.argv[1]), "w", newline="") as csv_file:
