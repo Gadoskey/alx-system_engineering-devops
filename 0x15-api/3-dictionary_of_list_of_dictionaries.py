74346269ce8fe3de93115f6e80e764ba26d81892
Gadoskey <gadoski321@gmail.com>
2024-07-30 05:28:49 -0700

    ok

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,7 +9,7 @@ import sys
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    users = requests.get(url + "users").json()
 
     with open("todo_all_employees.json", "w") as jsonfile:
         json.dump({
