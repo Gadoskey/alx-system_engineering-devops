0940d96709e4a1a9845791d54008642730e50f3a
Gadoskey <gadoski321@gmail.com>
2024-07-30 05:27:22 -0700

    ok

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,15 +10,13 @@ import sys
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    
-    with open("todo_all_employees.json", "w") as json_file:
+
+    with open("todo_all_employees.json", "w") as jsonfile:
         json.dump({
             u.get("id"): [{
                 "task": t.get("title"),
                 "completed": t.get("completed"),
                 "username": u.get("username")
             } for t in requests.get(url + "todos",
-                params={"userId": u.get("id")}).json()]
+                                    params={"userId": u.get("id")}).json()]
             for u in users}, jsonfile)
-
-    
