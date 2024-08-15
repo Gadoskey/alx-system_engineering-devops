2c8335ac8250c8bdd9f935da159729bc64b9fbbb
Gadoskey <gadoski321@gmail.com>
2024-08-15 00:58:16 -0700

    ok

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,22 +12,11 @@ def number_of_subscribers(subreddit):
     Args: subreddit (str): The name of the subreddit.
     Returns: int: The number of subscribers, or 0 if the subreddit is invalid
     """
-    # Set the URL for the Reddit API
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
-    # Set custom User-Agent to avoid Too Many Requests error
-    headers = {'User-Agent': 'MyRedditApp/0.1'}
-
-    try:
-        # Make the request without following redirects
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            # Return the number of subscribers from the API response
-            return data.get("data", {}).get("subscribers", 0)
-        else:
-            return 0
-
-    except requests.RequestException:
+    if subreddit is None or type(subreddit) is not str:
         return 0
-        # In case of a request failure, return 0
+    headers = {'User-Agent': '0x16-api_advanced: \
+            project: v1.0.0 (by /u/firdaus_cartoon_jr)'}
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    r = requests.get(url, headers=headers).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
