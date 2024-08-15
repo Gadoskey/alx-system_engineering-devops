659f3f48ef34270352c2a3cdeac2287568a48d16
Gadoskey <gadoski321@gmail.com>
2024-08-15 01:52:43 -0700

    1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,28 +8,30 @@ def top_ten(subreddit):
     Queries the Reddit API and prints the titles of
     the first 10 hot posts listed for a given subreddit.
     Args:
+    A
     subreddit (str): The name of the subreddit.
     """
+
     # Set the URL for the Reddit API
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    
+
     # Set custom User-Agent to avoid Too Many Requests error
     headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 \
       (by /u/firdaus_cartoon_jr)'}
-    
+
     try:
         # Make the request without following redirects
         response = requests.get(
                 url, headers=headers, allow_redirects=False, timeout=10)
-        
+
         # Check if the response is OK
         if response.status_code == 200:
             # Parse the response as JSON
             data = response.json().get('data', {})
-            
+
             # Get the list of articles
             children = data.get('children', [])
-            
+
             # Print the titles of the first 10 hot posts
             for i, article in enumerate(children[:10]):
                 print(f"{i + 1}. {article['data']['title']}")
